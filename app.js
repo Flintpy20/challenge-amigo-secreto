b2f737400458c02b3ca6b11d98968a6a0f8bccd8
Flintpy20 <flint.py20@gmail.com>
2025-03-18 04:50:47 -0300

    Adicionado funcionalidade adicionar amigo a lista

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let campo = document.getElementById('amigo').value;
+
+    if (campo === '') {
+        alert('Por favor, insira um nome.');
+        return;
+    } else {
+        amigos.push(campo);
+        limparCampo();
+        exibirAmigos();
+    }
+}
+
+function limparCampo() {
+    let campo = document.getElementById('amigo');
+    campo.value = '';
+}
+
+function exibirAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+
+        lista.appendChild(item);
+    }
+}
